47747264b2042cf98aeccd482e596bf55b4e29cb
Kelli <kellidavis320@gmail.com>
2024-04-27 00:07:16 -0400

    arithmetic operators example

---
+++ 2-Fundamentals/6_arithmetic_operators.py
@@ -0,0 +1,12 @@
+x = 11
+y = 2
+z = 4
+
+result = (x + y - z) ** (x % z)
+print(result)
+
+# Order of operations
+# brackets / parenthesis
+# exponents
+# multiplication and division
+# addition and subtraction
