14d52cf9c2d1a097fdb6d42af9af479d34ad7538
Kelli <kellidavis320@gmail.com>
2024-05-03 23:52:46 -0400

    updated title

--- 2-Fundamentals/caesar_cypher_2.py
+++ 2-Fundamentals/caesar_cypher_2.py
@@ -1,4 +1,4 @@
-def caesar_cipher(string, offset):
+def caesar_cypher(string, offset):
     # Define the alphabet as a list of characters in order
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
