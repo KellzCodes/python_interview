2317cd98874d2820228d800c5c10504e83a04210
Kelli <kellidavis320@gmail.com>
2024-05-19 02:47:35 -0400

    two number sum hash solution

---
+++ Data-Structures-and-Algorithms/Arrays/Easy-Array-Algorithms/Two-Number-Sum/hash_solution.py
@@ -0,0 +1,9 @@
+def twoNumberSum(array, targetSum):
+    nums = {}
+    for num in array:
+        match = targetSum - num
+        if match in nums:
+            return [match, num]
+        else:
+            nums[num] = True
+    return []
